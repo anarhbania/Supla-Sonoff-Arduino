31d62ff20fccbd23b510451575cd85067c4a01de
Marcin Bania <54779555+anarhbania@users.noreply.github.com>
2024-12-17 20:05:43 +0100

    Add files via upload

---
+++ Supla_Sonoff_Basic_R2/Supla_Sonoff_Basic_R2.ino
@@ -0,0 +1,45 @@
+#include <SuplaDevice.h>
+#include <supla/control/button.h>
+#include <supla/control/relay.h>
+#include <supla/device/status_led.h>
+#include <supla/device/supla_ca_cert.h>
+#include <supla/network/esp_web_server.h>
+#include <supla/network/esp_wifi.h>
+#include <supla/network/html/device_info.h>
+#include <supla/network/html/protocol_parameters.h>
+#include <supla/network/html/wifi_parameters.h>
+#include <supla/storage/eeprom.h>
+#include <supla/storage/littlefs_config.h>
+
+#define PINOUT_LED   13
+#define PINOUT_BUTTON 0
+#define PINOUT_RELAY 12
+
+Supla::Device::StatusLed statusLed(PINOUT_LED, true);
+Supla::Eeprom eeprom;
+Supla::ESPWifi wifi;
+Supla::EspWebServer suplaServer;
+Supla::LittleFsConfig configSupla;
+
+auto suplaButtonCfg = new Supla::Control::Button(PINOUT_BUTTON, true, true);
+auto suplaRelay = new Supla::Control::Relay(PINOUT_RELAY);
+
+void setup() 
+{
+  new Supla::Html::DeviceInfo(&SuplaDevice);
+  new Supla::Html::WifiParameters;
+  new Supla::Html::ProtocolParameters;
+
+  suplaButtonCfg->configureAsConfigButton(&SuplaDevice);
+  suplaButtonCfg->addAction(Supla::TOGGLE, suplaRelay, Supla::ON_CLICK_1);
+
+  SuplaDevice.setSuplaCACert(suplaCACert);
+  SuplaDevice.setSupla3rdPartyCACert(supla3rdCACert);
+
+  SuplaDevice.begin();
+}
+
+void loop()
+{
+  SuplaDevice.iterate();
+}
